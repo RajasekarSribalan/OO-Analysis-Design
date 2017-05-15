bc7f3fd22e5f06386029c9c0ade8a26257dc4f77
Rajasekar <rajasekar.sribalan@globallogic.com>
2017-05-15 18:14:47 +0530

    Automatic Dog Door openning System

---
+++ dogdoor/src/main/java/com/automatic/dogdoor/dogdoor/App.java
@@ -0,0 +1,13 @@
+package com.automatic.dogdoor.dogdoor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
